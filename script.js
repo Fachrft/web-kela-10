3dd5506f4b042c89e1f237bc88babe7dacab15b8
Fachrft <114207152+Fachrft@users.noreply.github.com>
2023-01-13 18:59:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-function submit() {
-    alert("pesan anda sudah terkirim")
-    alert('tapii boong')
-}
-
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
 
 const menuToggle = document.querySelector('.menu-toggle input')
 const nav = document.querySelector('nav ul')
+const form = document.querySelector('.c-pesan')
+const submit = document.querySelector('.submit')
 
 menuToggle.addEventListener ('click', function() {
     nav.classList.toggle('slide')
-    
+})
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('pesan anda sudah terkirim')
+    alert('tapi boong aokawokwokawo')
 })
