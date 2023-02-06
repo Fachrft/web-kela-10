512b4aaf7874caa5616b9c5cabbfcc8c34f46423
Fachrifat Zhafran <114207152+Fachrft@users.noreply.github.com>
2023-02-06 17:48:18 +0700

    menambahkan aksi a-navbar baru

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ const menuToggle = document.querySelector('.menu-toggle input')
 const nav = document.querySelector('nav ul')
 const form = document.querySelector('.c-pesan')
 const submit = document.querySelector('.submit')
+const aNavbar = document.querySelectorAll('.a-navbar')
+
 
 menuToggle.addEventListener ('click', function() {
     nav.classList.toggle('slide')
 })
+
+aNavbar.forEach((aNavbar) => {
+    aNavbar.addEventListener('click', function() {
+        nav.classList.remove('slide')
+    })
+})
+// aNavbar[0].addEventListener('click', function() {
+//     nav.classList.remove('slide')
+// })
+// aNavbar[1].addEventListener('click', function() {
+//     nav.classList.remove('slide')
+// })
+// aNavbar[2].addEventListener('click', function() {
+//     nav.classList.remove('slide')
+// })
+// aNavbar[3].addEventListener('click', function() {
+//     nav.classList.remove('slide')
+// })
+
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
